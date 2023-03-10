a2ef3206407bdf2ae87d39c4a5e489ccaa050882
Michael Trani <trani.mike@gmail.com>
2023-03-09 16:07:10 -0800

    Update lightAvgHours.sql

--- lightAvgHours.sql
+++ lightAvgHours.sql
@@ -1,6 +1,5 @@
 SELECT EXTRACT(HOUR FROM sensor_data.date) AS hour,
        AVG(sensor_data.light) AS average
 FROM sensor_data
-WHERE sensor_data.date BETWEEN '2023-03-05' AND '2023-03-08'
 GROUP BY hour
 ORDER BY hour;

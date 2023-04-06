6fe40d30490725f2099d0b54fa350415a02fcb67
Michael Trani <trani.mike@gmail.com>
2023-04-06 14:29:15 -0700

    Update serialToDBparser.py

--- serialToDBparser.py
+++ serialToDBparser.py
@@ -1,6 +1,5 @@
 import serial
 import psycopg2
-import datetime
 
 # Connect to the database
 conn = psycopg2.connect(dbname='environmentsensors', user='arduino', password='arduino', host='localhost')
@@ -12,6 +11,9 @@ ser = serial.Serial('/dev/ttyACM0', 9600)
 # create an empty dictionary to hold the sensor data
 sensor_data = {}
 cycle = None
+
+# create an empty dictionary to hold the plant data
+plant_data = {}
 
 # Read data from the serial port
 while True:
@@ -36,19 +38,27 @@ while True:
         value = value.strip()
 
         # Add the data point to the dictionary
-        sensor_data[name.lower().replace(' ', '')] = float(value)
+        if name.lower() == 'plantmoisture':
+            plant_num, moisture = value.split(',')
+            plant_data[f'p{plant_num}'] = int(moisture)
+        else:
+            sensor_data[name.lower().replace(' ', '')] = float(value)
 
-        if len(sensor_data) == 6:
+
+
+        if len(sensor_data) == 6 and len(plant_data) == 5:
             print(sensor_data)
+            print(plant_data)
 
-            # Truncate the milliseconds of the timestamp to two decimal places
-            now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
-
-            # Insert the data point into the database
-            cur.execute("INSERT INTO sensor_data (date, cycle, temperature, humidity, light, magnetic, gas, noiselevel) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
-                        (now, cycle, sensor_data.get('temperature', 0.0), sensor_data.get('humidity', 0.0), round(sensor_data.get('light', 0.0), 2), round(sensor_data.get('magnetic', 0.0), 2), round(sensor_data.get('gas', 0.0), 2), float(sensor_data.get('noiselevel', 0.0))))
+            # Insert the data point into sensor_data table
+            cur.execute(f"INSERT INTO sensor_data (date, cycle, temperature, humidity, light, magnetic, gas, noiselevel) VALUES (CURRENT_TIMESTAMP, %s, %s, %s, %s, %s, %s, %s)", (cycle, sensor_data['temperature'], sensor_data['humidity'], sensor_data['light'], sensor_data['magnetic'], sensor_data['gas'], sensor_data['noiselevel']))
             conn.commit()
 
-            # Reset the sensor data dictionary and cycle number
+            # Insert the data point into plant_data table
+            cur.execute(f"INSERT INTO plant_data (date, cycle, p0, p1, p2, p3, p4) VALUES (CURRENT_TIMESTAMP, %s, %s, %s, %s, %s, %s)", (cycle, plant_data.get('p0', None), plant_data.get('p1', None), plant_data.get('p2', None), plant_data.get('p3', None), plant_data.get('p4', None)))
+            conn.commit()
+
+            # Reset the sensor data and plant data dictionaries, and cycle number
             sensor_data = {}
+            plant_data = {}
             cycle = None

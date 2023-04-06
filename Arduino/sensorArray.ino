12398a7777213fbf86abcaa40141626526d53586
Michael Trani <trani.mike@gmail.com>
2023-04-06 15:21:20 -0700

    Update and rename sensorArray.ino to Arduino/sensorArray.ino

--- Arduino/sensorArray.ino
+++ Arduino/sensorArray.ino
@@ -1,12 +1,12 @@
 // Michael Trani February 2,2023
 #include "sensorArray.h" 
 
-#define clock 300000 // Five min interval, 1800000 for half hour
+#define timeInterval 300000 // Five min interval, 1800000 for half hour
 int cycle = 1;  		// Track how many times this has run
 sensorArray arrayPrime;
 
 void setup() {
-  Serial.begin(600);
+  Serial.begin(9600);
   sensors.begin();
   dht.begin();
   pinMode(MIC_LED_PIN, OUTPUT);
@@ -17,7 +17,7 @@ void loop() {
   Serial.print("# Cycle: ");
   Serial.println(cycle);
   arrayPrime.sensorRun();
-  delay(clock);  
+  delay(timeInterval);  
   ++cycle;
 }
 
